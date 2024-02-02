3425b4ec1cabb4fa64cf3438715ec96012049a25
nurululum <116478678+nurululum@users.noreply.github.com>
2024-02-02 22:38:06 +0700

    Update dag_etl.py

--- dags/dag_etl.py
+++ dags/dag_etl.py
@@ -1,28 +1,66 @@
-from sqlalchemy import create_engine
-import requests
-import os
-import pandas as pd 
-
-url_covid = 'http://103.150.197.96:5005/api/v1/rekapitulasi_v2/jabar/harian?level=kab%22'
-
-
-response = requests.get(url_covid)
-data= response.json()['data']['content']
-df = pd.DataFrame(data)
-df = pd.json_normalize(data)
-
-# Dataframe information
-print(df.info())
-
-mysql_username = 'your_username'
-mysql_password = 'your_password'
-mysql_host = 'mysql'
-mysql_port = 'port'
-mysql_database = 'your_database'
-
-mysql_conn_string = f"mysql+mysqlconnector://{mysql_username}:{mysql_password}@{mysql_host}:{mysql_port}/{mysql_database}"
-engine_mysql = create_engine(mysql_conn_string)
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy import DummyOperator 
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime
+from datetime import timedelta
 
 
-df.to_sql(name='covid_jabar', con=engine_mysql,index=False,if_exists='replace')
-print('DATA INSERTED TO MYSQL SUCCESSFULLY')
+
+path = '/opt/airflow/dags/modules'
+
+with DAG(
+        dag_id="dag_etl",
+        schedule_interval= "0 0 * * *",
+        start_date=datetime(2024, 1, 1),
+        catchup=False
+        ) as dag:
+
+        start = DummyOperator(
+            task_id='start')
+
+        data_extraction = BashOperator(
+            task_id = "data_extraction",
+            bash_command = f'python3 {path}/data_extraction.py',
+            execution_timeout=timedelta(minutes=5),
+            retries=2,
+            retry_delay=timedelta(minutes=5)
+        )
+
+
+        ddl_postgres = BashOperator(
+            task_id = "ddl_postgres",
+            bash_command = f'python3 {path}/ddl_postgres.py',
+            execution_timeout=timedelta(minutes=5),
+            retries=2,
+            retry_delay=timedelta(minutes=5)
+        )
+
+        generate_dim_table =  BashOperator(
+            task_id = "generate_dim_table",
+            bash_command = f'python3 {path}/generate_dim_table.py',
+            execution_timeout=timedelta(minutes=5),
+            retries=2,
+            retry_delay=timedelta(minutes=5)
+        )
+
+        generate_fact_province =  BashOperator(
+            task_id = "generate_fact_province",
+            bash_command = f'python3 {path}/generate_fact_province.py',
+            execution_timeout=timedelta(minutes=5),
+            retries=2,
+            retry_delay=timedelta(minutes=5)
+        )
+        
+        generate_fact_district = BashOperator(
+            task_id = "generate_fact_district",
+            bash_command = f'python3 {path}/generate_fact_district.py',
+            execution_timeout=timedelta(minutes=5),
+            retries=2,
+            retry_delay=timedelta(minutes=5)
+        )
+        
+        end = DummyOperator(
+            task_id='end')
+
+        start >> data_extraction >> ddl_postgres >> generate_dim_table >> [generate_fact_province,generate_fact_district] >> end
